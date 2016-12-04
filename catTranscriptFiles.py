6f0a8277222ee125f102f5ec1a139c6c19da42a8
Alec Heckert <alecheckert@Alecs-iMac.local>
2016-12-04 11:12:06 -0800

    changed catTranscriptFiles.py

--- catTranscriptFiles.py
+++ catTranscriptFiles.py
@@ -6,7 +6,7 @@ import pandas as pd
 import os
 import argparse
 
-def catTranscriptFiles(filenames, outfile):
+def catTranscriptFiles(filenames, outfile, column_drops=['startPhase', 'endPhase', 'constitutive', 'rank', 'protein_start', 'protein_end', 'entropy', 'exon_iupred', 'exon_iupred_stdev']):
 	'''
 	Find the full protein sequences for a set of transcript files and write them
 	to a new CSV.
@@ -15,6 +15,9 @@ def catTranscriptFiles(filenames, outfile):
 		filenames : list of string, names of transcript files or directories containing
 			transcript files
 		outfile : string, name of file to write full protein sequences to
+		column_drops : list of string, columns to remove from the final dataframe
+			(should be columns that are assumed to be exon- rather than transcript-
+			specific)
 
 	RETURNS
 		Pandas DataFrame, containing the outfile information
@@ -39,20 +42,22 @@ def catTranscriptFiles(filenames, outfile):
 		new_row = getFullProtein(target_files[0])
 		result = pd.DataFrame(columns=new_row.index)
 		result = result.append(new_row)
+		for drop_column in drop_columns:
+			result = result.drop(drop_column, 1)
 		result.to_csv(outfile, index=False)
 		print "Successfully concatenated file %s" % target_files[0]
 		return result
 	else:
 		new_row = getFullProtein(target_files[0])
-		print new_row
 		result = pd.DataFrame(columns=new_row.index)
 		result = result.append(new_row)
 		print "Successfully concatenated file %s" % target_files[0]
 		for f in target_files[1:]:
 			new_row = getFullProtein(f)
-			print new_row #debugging
 			result = result.append(new_row)
 			print "Successfully concatenated file %s" % f
+		for drop_column in drop_columns:
+			result = result.drop(drop_column, 1)
 		result.to_csv(outfile,index=False)
 		return result
 	for f in target_files:
@@ -63,7 +68,7 @@ def catTranscriptFiles(filenames, outfile):
 			continue
 	
 
-def getFullProtein(transcript_file, column_drops=['constitutive', 'startPhase', 'endPhase', 'protein_start', 'protein_end']):
+def getFullProtein(transcript_file, column_drops=['constitutive', 'startPhase', 'endPhase', 'protein_start', 'protein_end'], cat_columns=['protein', 'sequence']):
 	'''
 	Concatenate the exons from one transcript file into a single protein, and return
 	a DataFrame containing the full protein and various information about the protein.
@@ -72,6 +77,8 @@ def getFullProtein(transcript_file, column_drops=['constitutive', 'startPhase', 
 		transcript_file : string, name of the target file
 		column_drops : list of string, columns to be selectively excluded from the
 			final dataframe
+		cat_columns : list of string, columns to be concatenated (includes protein
+			and sequence columns)
 
 	RETURNS
 		Pandas DataFrame, containing full protein information (one row)
@@ -85,17 +92,20 @@ def getFullProtein(transcript_file, column_drops=['constitutive', 'startPhase', 
 		print "Could not read file %f" % f
 		return False
 	full_protein = ''
+	full_sequence = ''
 	for i in f.index:
 		try:
 			full_protein += f.ix[i,'protein']
+			full_sequence += f.ix[i,'sequence']
 		except TypeError:
 			continue
 	#do some violence to the dataframe
 	f.ix[1,'protein']=full_protein
+	f.ix[1,'sequence']=full_sequence
 	if len(f.index)==1:
 		return f
 	else:
-		columns = [j for j in f.columns if j!='protein']
+		columns = [j for j in f.columns if j not in cat_columns]
 		for column in columns:
 			if not all([f.ix[i,column]==f.ix[1,column] for i in range(2, len(f.index))]):
 				f = f.drop(column, 1)
